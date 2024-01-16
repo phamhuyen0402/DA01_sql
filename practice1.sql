e1bb40aa2bce23d3e8dd2dead0085f1dd6f9b2c5
phamhuyen0402 <64645006+phamhuyen0402@users.noreply.github.com>
2024-01-16 22:55:43 +0700

    Create practice1.sql

---
+++ practice1.sql
@@ -0,0 +1,15 @@
+1. select name from city where population > 120000 and countrycode like 'USA';
+2. select * from city where countrycode like 'JPN';
+3. select city,state from station;
+4. select distinct city from station where (city like 'A%') or (city like 'E%') or (city like 'I%') or (city like 'O%') or (city like 'U%');
+5. select distinct city from station where (city like '%a') or (city like '%e') or (city like '%i') or (city like '%o') or (city like '%u');
+6. select distinct city from station where not((city like 'A%') or (city like 'E%') or (city like 'I%') or (city like 'O%') or (city like 'U%'));
+7. select name from employee order by name;
+8. select name from employee where salary > 2000 and months <10 order by employee_id;
+9. select product_id from products where low_fats ='Y' and recyclable ='Y';
+10. select name from customer where referee_id <> 2;
+11. select name,population,area from world where area > 3000000 or population > 25000000;
+12. select distinct author_id from views where author_id = viewer_id order by id asc;
+13. SELECT part,assembly_step FROM parts_assembly where finish_date is null;
+14. select * from lyft_drivers where yearly_salary>=30000 or yearly_salary<=70000;
+15. select advertising_channel from uber_advertising where money_spent > 100000 and year = 2019;
